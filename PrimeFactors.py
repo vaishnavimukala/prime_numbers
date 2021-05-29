91a15312fc7a3d52efb37ccd9680dbed41914520
vaishnavimukala <52392339+vaishnavimukala@users.noreply.github.com>
2021-05-29 19:23:26 +0530

    Update PrimeFactors.py

--- PrimeFactors.py
+++ PrimeFactors.py
@@ -1,21 +1,3 @@
-#def prime(n):
-#   count=0
-#   for i in range(2,n+1):
-#       if n%i==0:
-#           count+=1
-#   if count==1:
-#       return True
-#   else:
-#       return False
-#n=int(input())
-#l=[]
-#for i in range(1,n+1):#find out factors of n
-#   if n%i==0:
-#        l.append(i)
-#print(l)        
-#for i in l:
-#    if prime(i):#check if factor is prime or not
-#print(i,end=' ')
 N=1000
 sieve=[1]*1000
 def gen_seive():
@@ -31,9 +13,6 @@ for i in range(1,n+1):
         if sieve[i]==1:
             print(i,end=' ')
             
-#if sieve[n]==1:
-#        print('prime ')
-#else:
-#        print("not prime")
+
     
         
