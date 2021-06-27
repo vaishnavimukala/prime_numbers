c5853760801b3384132c435ca7328ba4b460e7dd
vaishnavimukala <52392339+vaishnavimukala@users.noreply.github.com>
2021-06-27 14:13:07 +0530

    Add files via upload

---
+++ Sum_Two_primes_is_prime.py
@@ -0,0 +1,27 @@
+N=1000
+sieve=[1]*1000
+def gen_sieve():
+    sieve[0]=sieve[1]=0
+    for i in range(2,int(N**0.5)+1):
+        if sieve[i]==1:
+            for j in range(i*i,N,i):
+                sieve[j]=0
+gen_sieve()
+n=int(input())
+for i in range(n-1,0,-1):
+    if sieve[i]==1:
+        k=i
+        break
+#print('Smallest nearest Prime',k)    
+j=n+1       
+while j>n:      
+    if sieve[j]==1:
+        h=j
+        break
+    j=j+1
+#print('Largest nearest Prime',h)
+print(h+k)    
+if sieve[h+k]==1:
+    print('Sum of non-consecutive is prime')
+else:
+    print('Sum of non-consecutive is not prime')
